542a9b8b5efb12c9dd8b455512909118c652df87
Rohit Pardeshi <pardeshirohit080294@gmail.com>
2025-03-22 20:32:26 +0530

    My first comment

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a[]={1,3,5,7,9};
+        int b[]={2,4,6,8,10};
+        int c[];
+
+        Set<Integer> setDemo=new HashSet<>();
+
+        for(int i=0;i<a.length;i++)
+        {
+            setDemo.add(a[i]);
+        }
+        for(int i=0;i<b.length;i++)
+        {
+            setDemo.add(b[i]);
+        }
+        System.out.println(setDemo);
+	//Testing line
+    }
+}
